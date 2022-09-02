6a160e1ac568740f074695d6934e9cb4bea46b48
Esraa <esraa.ramadan2345@gmail.com>
2022-09-02 19:42:59 +0200

    AddForgetPasswordApiv5

--- modules/post/controller/post.js
+++ modules/post/controller/post.js
@@ -10,7 +10,7 @@ import UserDataModel from "../../../general/dto/userDataModel.js";
 const createPost = async (req, res) => {
     const { title } = req.body
     if (req.fileErr) {
-        res.json(new ResponseModel(req.fileErr,false, req.t('InvalidFormat')  ))
+        res.json(new ResponseModel(req.files,false, req.t('InvalidFormat')  ))
     } else {
         const imageURL = [];
         req.files.forEach(file => {
