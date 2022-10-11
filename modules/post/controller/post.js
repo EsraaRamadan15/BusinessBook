9d4b2e8036aaa855fa701a03b1b3127177b87ef7
Esraa <esraa.ramadan2345@gmail.com>
2022-10-11 15:54:17 +0200

    adjustNewDb

--- modules/post/controller/post.js
+++ modules/post/controller/post.js
@@ -5,13 +5,13 @@ import postModel from "../../../DB/models/postModels/post.js";
 import userModel from "../../../DB/models/user.js";
 import paginate from "../../../service/paginate.js";
 
-import ResponseModel  from "../../../general/dto/responseModel.js";
-import Post  from "../dto/post.js";
+import ResponseModel from "../../../general/dto/responseModel.js";
+import Post from "../dto/post.js";
 import UserDataModel from "../../../general/dto/userDataModel.js";
 const createPost = async (req, res) => {
     const { title } = req.body
     if (req.fileErr) {
-        res.status(400).json(new ResponseModel(req.files,false, req.t('InvalidFormat')  ))
+        res.status(400).json(new ResponseModel(req.files, false, req.t('InvalidFormat')))
     } else {
         //console.log(req.file)
         const imageURL = [];
@@ -20,14 +20,14 @@ const createPost = async (req, res) => {
         });
         const newPost = new postModel({ title, media: imageURL, createdBy: req.userId })
         const savedPost = await newPost.save()
-        res.status(201).json(new ResponseModel(true,true,""))
+        res.status(201).json(new ResponseModel(true, true, ""))
     }
 }
 
 const UploadFile = async (req, res) => {
     //const { title } = req.body
     if (req.fileErr) {
-        res.status(400).json(new ResponseModel(req.file,false, req.t('InvalidFormat')  ))
+        res.status(400).json(new ResponseModel(req.file, false, req.t('InvalidFormat')))
     } else {
         console.log(req.file)
         const imageURL = [];
@@ -35,62 +35,47 @@ const UploadFile = async (req, res) => {
         //     imageURL.push(`${req.finalDestination}/${file.filename}`)
         // });
         imageURL.push(`${req.file.finalDestination}/${req.file.filename}`)
-        res.status(201).json(new ResponseModel(req.file,true,""))
+        res.status(201).json(new ResponseModel(req.file, true, ""))
     }
 }
 
 const getAllPosts = async (req, res) => {
-   const { page, size } = req.query
-   const { skip, limit } = paginate(1,100)
-     let userFollowing= await userModel.findById( req.userId ).select('following')
-     userFollowing=userFollowing.following.map(x => x.toString())
-
-
-     let  followerPostsDb = await postModel.find({ "createdBy": { "$in": userFollowing } } ).sort([['createdAt', -1]]).select('_id title media createdAt likes comments').populate([      
-        {
-            path: 'createdBy',
-            select: "_id  firstName lastName personalImage"
-         },{
-            path: 'likes',
-            match: {
-                userId: req.userId
-            },
-            select: "react"
-         }
-        
-    ])
-
-    // let  otherPostsDb = await postModel.find(  {
-    //     $and: [{"createdBy": { "$ne": userFollowing } } , {"createdBy": { "$ne": req.userId } }
-    //     ]}).sort([['createdAt', -1]]).select('_id title media createdAt likes comments').populate([      
-    //     {
-    //         path: 'createdBy',
-    //         select: "_id  firstName lastName personalImage"
-    //      },{
-    //         path: 'likes',
-    //         match: {
-    //             userId: req.userId
-    //         },
-    //         select: "react"
-    //      }
-        
-    // ])
-  // var allPosts= followerPostsDb.concat(otherPostsDb)
-    var Posts=[];
-    alfollowerPostsDblPosts.forEach(function(obj){
-        let selfReact=null,reactId=null;;
-        if (obj.likes.length >0)
-        {
-            selfReact=obj.likes[0].react;
-            reactId=obj.likes[0]._id;
-        }
-        Posts.push(
-        new Post(obj._id,obj.title,obj.media,obj.createdAt,obj.likes.length,obj.comments.length,selfReact,reactId,
-            new UserDataModel(obj.createdBy._id,obj.createdBy.firstName +" "+ obj.createdBy.lastName,obj.createdBy.personalImage))
-       )
-    });
-    res.status(200).json(new ResponseModel(allPosts,true,""))
+    const { page, size } = req.query
+    const { skip, limit } = paginate(1, 100)
+    var follwedByPosts = await GetPostsForOtherUsers(req.userId);
+    var rearrangedPosts = await ReArrangePostsSort(follwedByPosts, req.userId);
+    res.status(200).json(new ResponseModel(rearrangedPosts, true, ""))
 }
 
 
-export { createPost ,getAllPosts,UploadFile}
+async function GetPostsForOtherUsers(userId) {
+
+    let followerPostsDb = await postModel.find({ "createdBy": { "$ne": userId } }).sort([['createdAt', -1]]).select('_id title media createdAt likes comments').populate([
+        {
+            path: 'createdBy',
+            select: "_id  firstName lastName personalImage"
+        }, {
+            path: 'likes',
+            match: {
+                userId: userId
+            },
+            select: "react"
+        }
+
+    ])
+
+    return followerPostsDb;
+}
+
+async function ReArrangePostsSort(posts, userId) {
+    let userFollowing = await userModel.findById(userId).select('following')
+    userFollowing = userFollowing.following.map(x => x.toString())
+    var followedUsers = posts.filter(x => userFollowing.includes(x.createdBy._id.toString()));
+    var notFollowedUsers = posts.filter(x => !userFollowing.includes(x.createdBy._id.toString()));
+
+    var posts = followedUsers.concat(notFollowedUsers);
+
+    return posts;
+}
+
+export { createPost, getAllPosts, UploadFile }
